7952ccfa43b39fc1d859588b4e0b7aa82cb81c25
Ivan Ortega <iortega@newrelic.com>
2019-09-04 23:23:15 +0200

    feat: remove react-create-app

--- app/src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Navbar } from './Navbar';
-export { default as CreativeCard } from './CreativeCard';
-export { default as Grid } from './Grid';
-export { default as FilesList } from './FilesList';
-export { default as FileCard } from './FileCard';
-export { default as FilesCommandBar } from './FilesCommandBar';
-export { default as Metadata } from './Metadata';
